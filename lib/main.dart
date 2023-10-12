2e323de9402a90599617753f94ee8c21d219e242
Dre_baba <akinyedre007@gmail.com>
2023-10-12 18:17:58 +0100

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:team_vibrant_breakout/appTheme/colors.dart';
 import 'package:team_vibrant_breakout/screens/gameScreen.dart';
